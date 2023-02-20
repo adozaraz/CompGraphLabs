224079a1130f4b00347a25b6cf7846abca330924
adozaraz <minecraft-no.adozaraz@yandex.ru>
2023-02-20 19:43:24 +0400

    Небольшой рефактор

--- lab1.py
+++ lab1.py
@@ -2,6 +2,8 @@ import numpy as np
 from PIL import Image
 from Point import Point
 from operator import methodcaller
+from math import pi, cos, sin
+
 
 # Задание 1
 
@@ -147,3 +149,60 @@ def drawPolygons(H, W, nodes, polygons, transformNumber):
         image = drawLine(point1, point2, image, [255, 255, 255], 3)
 
     return image
+
+
+def task1(H, W):
+    print('# Task 1')
+    print('Creating Image Matrices')
+    imgs = createImageMatrix(H, W)
+    print('Generating Image Files')
+    i = 0
+    for img in imgs:
+        saveImage(img, i)
+        i += 1
+    print('Finished')
+
+def task2(H, W):
+    print('\n\n# Task 2')
+    imgs = [np.zeros((H, W, 3), dtype=np.uint8) for i in range(4)]
+    print('Drawing Lines')
+    for j in range(len(imgs)):
+        print(f'Algorithm {j}')
+        for i in range(13):
+            startingPoint = Point(100, 100)
+            alpha = 2 * pi * i / 13
+            point = Point(int(100 + 95 * cos(alpha)), int(100 + 95 * sin(alpha)))
+            print(f'Point {i}')
+            print(f'({startingPoint.x}, {startingPoint.y}), ({point.x}, {point.y})')
+            imgs[j] = drawLine(startingPoint, point, imgs[j], [255, 255, 255], j)
+
+    print('Saving images')
+    i = 0
+    for img in imgs:
+        saveImage(img, f'star{i}')
+        i += 1
+
+
+def task3():
+    print('\n\n# Task 3')
+    nodes = getNodesFromFile('objects/fox.obj')
+    for i in nodes:
+        print(i)
+
+
+def task4(H, W, filePath='objects/rabbit.obj', color=None, fileName='rabbit', transformNumber=4000):
+    if color is None:
+        color = [255, 255, 255]
+    nodes = getNodesFromFile(filePath)
+    image = drawNodes(nodes, H, W, color, transformNumber)
+    saveImage(image, f'{fileName}', rotate=True)
+
+
+
+
+
+def task6(H, W, filePath='objects/rabbit.obj', fileName='RabbitPolygon', transformNumber=4000):
+    nodes = getNodesFromFile(filePath, toPoint=True)
+    polygons = getPolygons(filePath)
+    image = drawPolygons(H, W, nodes, polygons, transformNumber)
+    saveImage(image, fileName, rotate=True)
